fe5985d98fd2f290ab5e03326fbcd485771b1993
Chimco26 <chimco26@gmail.com>
2024-09-16 17:50:13 +0200

    adding urlencoded for searchfield

--- app/src/main/java/com/example/obivapp2/network/ApiService.kt
+++ app/src/main/java/com/example/obivapp2/network/ApiService.kt
@@ -1,7 +1,11 @@
 import retrofit2.http.GET
 import retrofit2.http.Url
 import okhttp3.ResponseBody
+import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 
 interface ApiService {
     @GET("m4tbnbwawm88e0q/home/obivap")
@@ -9,4 +13,8 @@ interface ApiService {
 
     @GET
     suspend fun getVideo(@Url url: String): Response<ResponseBody>
+
+    @FormUrlEncoded
+    @POST("m4tbnbwawm88e0q/home/xxx")  // Complète l'URL ici
+    fun searchMovie(@Field("searchword") searchword: String): Call<ResponseBody>
 }
